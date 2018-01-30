59dd4cf14e997d6e8e495ee79481dfdaeb5aa251
Johannes Jasper <johannes.jasper@nexenio.com>
2018-01-30 17:36:30 +0100

    Replace arrays with List in message package

--- fido-uaf-core/src/main/java/com/nexenio/fido/uaf/core/operation/registration/RegistrationRequestGeneration.java
+++ fido-uaf-core/src/main/java/com/nexenio/fido/uaf/core/operation/registration/RegistrationRequestGeneration.java
@@ -16,17 +16,18 @@
 
 package com.nexenio.fido.uaf.core.operation.registration;
 
-import com.nexenio.fido.uaf.core.crypto.BCrypt;
-import com.nexenio.fido.uaf.core.crypto.Notary;
+import com.nexenio.fido.uaf.core.crypto.*;
 import com.nexenio.fido.uaf.core.message.*;
-import com.nexenio.fido.uaf.core.util.PolicyUtil;
+import com.nexenio.fido.uaf.core.util.*;
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.commons.codec.binary.Base64;
 
 public class RegistrationRequestGeneration {
 
     public static final String APP_ID = "https://uaf.ebay.com/uaf/facets";
     private String appId = APP_ID;
-    private String[] acceptedAaids;
+    private List<String> acceptedAaids = new ArrayList<>();
 
     public RegistrationRequestGeneration() {
 
@@ -36,7 +37,7 @@ public class RegistrationRequestGeneration {
         this.appId = appId;
     }
 
-    public RegistrationRequestGeneration(String appId, String[] acceptedAaids) {
+    public RegistrationRequestGeneration(String appId, List<String> acceptedAaids) {
         this.appId = appId;
         this.acceptedAaids = acceptedAaids;
     }
@@ -44,23 +45,21 @@ public class RegistrationRequestGeneration {
     public RegistrationRequest createRegistrationRequest(String username,
                                                          Notary notary) {
         String challenge = generateChallenge();
-        String serverDataString = generateServerData(username, challenge,
-                notary);
+        String serverDataString = generateServerData(username, challenge, notary);
         return createRegistrationRequest(username, serverDataString, challenge);
     }
 
     private String generateServerData(String username, String challenge,
                                       Notary notary) {
         String dataToSign = Base64.encodeBase64URLSafeString(("" + System
-                .currentTimeMillis()).getBytes())
-                + "."
-                + Base64.encodeBase64URLSafeString(username.getBytes())
-                + "."
-                + Base64.encodeBase64URLSafeString(challenge.getBytes());
+            .currentTimeMillis()).getBytes())
+            + "."
+            + Base64.encodeBase64URLSafeString(username.getBytes())
+            + "."
+            + Base64.encodeBase64URLSafeString(challenge.getBytes());
         String signature = notary.sign(dataToSign);
 
-        return Base64.encodeBase64URLSafeString((signature + "." + dataToSign)
-                .getBytes());
+        return Base64.encodeBase64URLSafeString((signature + "." + dataToSign).getBytes());
     }
 
     private RegistrationRequest createRegistrationRequest(String username,
--- fido-uaf-core/src/main/java/com/nexenio/fido/uaf/core/util/PolicyUtil.java
+++ fido-uaf-core/src/main/java/com/nexenio/fido/uaf/core/util/PolicyUtil.java
@@ -1,11 +1,13 @@
 package com.nexenio.fido.uaf.core.util;
 
-import com.nexenio.fido.uaf.core.message.MatchCriteria;
-import com.nexenio.fido.uaf.core.message.Policy;
+import com.google.common.collect.Lists;
+import com.nexenio.fido.uaf.core.message.*;
+import java.util.List;
+import java.util.stream.*;
 
 public abstract class PolicyUtil {
 
-    public static Policy constructAuthenticationPolicy(String[] acceptedAaids) {
+    public static Policy constructAuthenticationPolicy(List<String> acceptedAaids) {
         if (acceptedAaids == null) {
             return null;
         }
@@ -14,14 +16,10 @@ public abstract class PolicyUtil {
         return policy;
     }
 
-    public static MatchCriteria[][] createMatchCriteriaFromAaids(String[] aaids) {
-        MatchCriteria[][] aaidMatchCriteria = new MatchCriteria[aaids.length][1];
-        for (int i = 0; i < aaidMatchCriteria.length; i++) {
-            MatchCriteria matchCriteria = new MatchCriteria();
-            matchCriteria.setAaids(new String[]{aaids[i]});
-            aaidMatchCriteria[i] = new MatchCriteria[]{matchCriteria};
-        }
-        return aaidMatchCriteria;
+    public static List<List<MatchCriteria>> createMatchCriteriaFromAaids(List<String> aaids) {
+        return aaids.stream()
+                    .map(a -> Lists.newArrayList(new MatchCriteria().setAaids(Lists.newArrayList(a))))
+                    .collect(Collectors.toList());
     }
 
 }
